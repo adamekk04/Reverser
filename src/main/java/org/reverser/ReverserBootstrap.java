a4a1c274b09a2a82a163778ddbcedad69314383d
adamekk04 <157727680+adamekk04@users.noreply.github.com>
2026-09-12 18:40:43 +0200

    Added plugin template

---
+++ src/main/java/org/reverser/ReverserBootstrap.java
@@ -0,0 +1,12 @@
+package org.reverser;
+
+import io.papermc.paper.plugin.bootstrap.BootstrapContext;
+import io.papermc.paper.plugin.bootstrap.PluginBootstrap;
+
+class ReverserBootstrap implements PluginBootstrap {
+
+    @Override
+    public void bootstrap(final BootstrapContext context) {
+        // Plugin bootstrap logic
+    }
+}

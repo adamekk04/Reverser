05ad0aa50661e0d51f39dd4fab99ca65ef635b56
adamekk04 <157727680+adamekk04@users.noreply.github.com>
2026-09-13 09:55:38 +0200

    Bunch of optimalization changes
    Temperature and humidity uses NMS classes
    
    Stopped creating ServerLevel every iteration

--- src/main/java/org/reverser/Reverser.java
+++ src/main/java/org/reverser/Reverser.java
@@ -25,7 +25,7 @@ public final class Reverser extends JavaPlugin {
         try {
             seed = Long.parseLong(seedFromArg);
         } catch (NumberFormatException e) {
-            getLogger().log(Level.SEVERE, "Invalid seed: " + seedFromArg + ", using ranodm one");
+            getLogger().warning("Invalid seed: " + seedFromArg + ", using ranodm one");
             Random random = new Random();
             seed = random.nextLong();
         }
@@ -54,47 +54,39 @@ public final class Reverser extends JavaPlugin {
             getLogger().severe("Something went wrong while creating files");
         }
 
-        // Fetching temperature and humidity for 100x100 block radius by using PaperAPI
+        // Creating world by using PaperAPI
         WorldCreator worldCreator = new WorldCreator("reverser");
         worldCreator.seed(seed);
         World world = worldCreator.createWorld();
         getLogger().info("Created world");
 
+        // Fetching temperature, humidity, continentness, erosion, weirdness and depth by accessing NMS
         try (
                 FileWriter temperatureFW = new FileWriter(temperature);
-                FileWriter humidityFW = new FileWriter(humidity)
-        ) {
-            for (int x = -100; x < 100; x++) {
-                for (int z = -100; z < 100; z++) {
-                    for (int y = -64; y < 320; y++) {
-                        String prefix = x + ";" + y + ";" + z + ";";
-
-                        temperatureFW.write(prefix + world.getTemperature(x, y, z) + "\n");
-                        humidityFW.write(prefix + world.getHumidity(x, y, z) + "\n");
-                    }
-                }
-            }
-        } catch (IOException e) {
-            getLogger().log(Level.SEVERE, "Something went wrong while writing into files.", e);
-        }
-        getLogger().info("Finished writing temperature and humidity.");
-
-        // Fetching continentness, erosion, weirdness and depth by accessing NMS
-        try (
+                FileWriter humidityFW = new FileWriter(humidity);
                 FileWriter continentnessFW = new FileWriter(continentness);
                 FileWriter erosionFW = new FileWriter(erosion);
                 FileWriter weirdnessFW = new FileWriter(weirdness);
                 FileWriter depthFW = new FileWriter(depth)
         ) {
+            // Creating NMS World
+            CraftWorld craftWorld = (CraftWorld) world;
+            ServerLevel serverLevel = craftWorld.getHandle();
+            RandomState randomState = serverLevel.getChunkSource().randomState();
+            Climate.Sampler sampler = randomState.sampler();
+
             for (int x = -100; x < 100; x++) {
                 for (int z = -100; z < 100; z++) {
                     for (int y = -64; y < 320; y++) {
                         String prefix = x + ";" + y + ";" + z + ";";
+                        DensityFunction.FunctionContext context = new DensityFunction.SinglePointContext(x, y, z);
 
-                        continentnessFW.write(prefix + getContinentness(x, y, z, world) + "\n");
-                        erosionFW.write(prefix + getErosion(x, y, z, world) + "\n");
-                        weirdnessFW.write(prefix + getWeirdness(x, y, z, world) + "\n");
-                        depthFW.write(prefix + getDepth(x, y, z, world) + "\n");
+                        temperatureFW.write(prefix + sampler.temperature().compute(context) + "\n");
+                        humidityFW.write(prefix + sampler.humidity().compute(context) + "\n");
+                        continentnessFW.write(prefix + sampler.continentalness().compute(context) + "\n");
+                        erosionFW.write(prefix + sampler.erosion().compute(context) + "\n");
+                        weirdnessFW.write(prefix + sampler.weirdness().compute(context) + "\n");
+                        depthFW.write(prefix + sampler.depth().compute(context) + "\n");
                     }
                 }
             }
@@ -109,43 +101,5 @@ public final class Reverser extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-    }
-
-    private double getContinentness(int x, int y, int z, World world) {
-        RandomState randomState = makeNMSWorld(world);
-        Climate.Sampler sampler = randomState.sampler();
-        DensityFunction.FunctionContext context = new DensityFunction.SinglePointContext(x, y, z);
-
-        return sampler.continentalness().compute(context);
-    }
-
-    private double getErosion(int x, int y, int z, World world) {
-        RandomState randomState = makeNMSWorld(world);
-        Climate.Sampler sampler = randomState.sampler();
-        DensityFunction.FunctionContext context = new DensityFunction.SinglePointContext(x, y, z);
-
-        return sampler.erosion().compute(context);
-    }
-
-    private double getWeirdness(int x, int y, int z, World world) {
-        RandomState randomState = makeNMSWorld(world);
-        Climate.Sampler sampler = randomState.sampler();
-        DensityFunction.FunctionContext context = new DensityFunction.SinglePointContext(x, y, z);
-
-        return sampler.weirdness().compute(context);
-    }
-
-    private double getDepth(int x, int y, int z, World world) {
-        RandomState randomState = makeNMSWorld(world);
-        Climate.Sampler sampler = randomState.sampler();
-        DensityFunction.FunctionContext context = new DensityFunction.SinglePointContext(x, y, z);
-
-        return sampler.depth().compute(context);
-    }
-
-    private RandomState makeNMSWorld(World world) {
-        CraftWorld craftWorld = (CraftWorld) world;
-        ServerLevel serverLevel = craftWorld.getHandle();
-        return serverLevel.getChunkSource().randomState();
     }
 }
